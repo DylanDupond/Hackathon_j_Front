9f7e373c706be0b1cddb1e041b87b8cc56733073
Dylan <neorockdevil@hotmail.fr>
2022-11-23 17:32:26 +0100

    Pagination OK

--- src/app/festival-component/festival-component.component.ts
+++ src/app/festival-component/festival-component.component.ts
@@ -1,36 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
+import { Component} from '@angular/core';
 import {FestivalService} from "../festival.service";
-import {Festival} from "../Festival";
 
 @Component({
   selector: 'app-festival-component',
   templateUrl: './festival-component.component.html',
   styleUrls: ['./festival-component.component.css']
 })
-export class FestivalComponentComponent implements OnInit {
+export class FestivalComponentComponent{
 
-  festivals: Observable<any>;
-  fields : any;
+  festivals: any;
   page:number=1;
   count:number=0;
   tableSize:number=9;
   constructor(private realTimeDatabase: FestivalService) {
-    this.festivals =  realTimeDatabase.getFestival();
-    this.GetData();
   }
 
+  ngOnInit():void{
+    this.GetData();
+  }
   GetData(){
-    this.realTimeDatabase.getFestival().subscribe(res => this.fields = res);
-    console.log(this.fields);
+    this.realTimeDatabase.getFestival().subscribe((res) => {this.festivals = res;})
   }
 
   OnDataChange(event:any){
     this.page = event;
     this.GetData();
   }
-
-  ngOnInit(): void {
-  }
-
 }
